68f872898c69e7482d7464df864bf60a86444bb8
dongkw <1038459754@qq.com>
2019-12-05 16:51:12 +0800

    module

---
+++ service/user-service/src/main/java/xyz/jecy/user/UserApplication.java
@@ -0,0 +1,17 @@
+package xyz.jecy.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Author dkw[dongkewei@xinzhili.cn]
+ * @data 2019/12/5 4:27 下午
+ */
+@SpringBootApplication
+public class UserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserApplication.class, args);
+  }
+
+}
